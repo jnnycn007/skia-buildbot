28b3e282a0ac0e8d741644d8fd98c5130814e37d
Eric Boren <borenet@google.com>
2026-02-25 16:04:28 +0000

    [cdn] Add automatic backup functionality
    This gives us a way to back things up without doing it manually with
    breakglass, which should be safer.
    
    A possibly nicer solution might be to check in a file containing the
    URLs and digests and have it built into the docker image. An even more
    robust solution would be to add a UI or CLI for requesting one-off
    backups, but that would require persistent storage of some kind to
    ensure consistency. I went with the lowest-friction path for now.
    
    Change-Id: I2843e690c20896029d9e664419f01a576127e055
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1174702
    Reviewed-by: Kaylee Lubick <kjlubick@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- cdn/go/cdn/main.go
+++ cdn/go/cdn/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
 	"errors"
 	"flag"
 	"fmt"
@@ -14,6 +15,7 @@ import (
 	"github.com/rs/cors"
 	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/skerr"
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/util"
 	"golang.org/x/oauth2/google"
@@ -30,6 +32,11 @@ var (
 
 	// Global GCS client.
 	gcsClient *storage.Client
+
+	// These URLs get backed up to GCS on startup, if they aren't already present.
+	backupUrlToSha256 = map[string]string{
+		"https://snapshot-cloudflare.debian.org/archive/debian/20260201T022025Z/pool/main/c/ca-certificates/ca-certificates_20250419_all.deb": "ef590f89563aa4b46c8260d49d1cea0fc1b181d19e8df3782694706adf05c184",
+	}
 )
 
 func validatePath(path string) error {
@@ -110,6 +117,13 @@ func main() {
 		sklog.Fatal(err)
 	}
 
+	// Run file backup in a separate goroutine to avoid downtime if it fails.
+	go func() {
+		if err := ensureBackups(ctx, httpClient, backupUrlToSha256); err != nil {
+			sklog.Errorf("Failed backups: %s", err)
+		}
+	}()
+
 	serverURL := "https://" + *host
 	if *local {
 		serverURL = "http://" + *host + *port
@@ -126,4 +140,59 @@ func main() {
 	}
 	sklog.Infof("Ready to serve on %s", serverURL)
 	sklog.Fatal(http.ListenAndServe(*port, h))
+}
+
+func ensureBackups(ctx context.Context, httpClient *http.Client, urlToSha256 map[string]string) error {
+	sklog.Infof("Backing up specified URLs...")
+	for url, sha256 := range urlToSha256 {
+		if err := maybeBackupURL(ctx, httpClient, url, sha256); err != nil {
+			return skerr.Wrapf(err, "failed to back up %q", url)
+		}
+	}
+	sklog.Infof("Done backing up specified URLs.")
+	return nil
+}
+
+func maybeBackupURL(ctx context.Context, httpClient *http.Client, url, expectSha256 string) error {
+	// Per storage API docs, if the context is cancelled before Writer.Close()
+	// is called, the writes are not saved. We'll use this to ensure that the
+	// object doesn't end up existing if the digest doesn't match the
+	// expectation.
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	obj := gcsClient.Bucket(*bucket).Object(expectSha256)
+	if _, err := obj.Attrs(ctx); err == nil {
+		// The object already exists; nothing to do.
+		return nil
+	}
+
+	// We'll write the object while computing the digest.
+	const bufSize = 1024 * 1024
+	objWriter := obj.NewWriter(ctx)
+	objWriter.ChunkSize = bufSize
+	hash := sha256.New()
+	w := io.MultiWriter(objWriter, hash)
+
+	// Read the URL.
+	resp, err := httpClient.Get(url)
+	if err != nil {
+		return skerr.Wrap(err)
+	}
+	defer resp.Body.Close()
+
+	// Perform the copy.
+	buf := make([]byte, bufSize)
+	if _, err := io.CopyBuffer(w, resp.Body, buf); err != nil {
+		return skerr.Wrap(err)
+	}
+
+	// Ensure that the digest matches the expectation.
+	actualSha256 := fmt.Sprintf("%x", hash.Sum(nil))
+	if actualSha256 != expectSha256 {
+		return skerr.Fmt("incorrect sha256 digest for %q: expected %s but got %s", url, expectSha256, actualSha256)
+	}
+	sklog.Infof("Backed up %s with digest %s", url, expectSha256)
+
+	return skerr.Wrap(objWriter.Close())
 }
